ccfe62f4a9554af4aea7844460fe9a2e9399e5c4
vrana28 <vranjesdjordje@gmail.com>
2021-03-15 14:47:21 +0100

    Testiran interfejs BibliotekaInterface i klasa Biblioteka

---
+++ test/biblioteka/BibliotekaTest.java
@@ -0,0 +1,26 @@
+package biblioteka;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import biblioteka.interfejs.BibliotekaInterfaceTest;
+
+class BibliotekaTest extends BibliotekaInterfaceTest {
+
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		biblioteka = new Biblioteka();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		biblioteka = null;
+	}
+
+	
+}
